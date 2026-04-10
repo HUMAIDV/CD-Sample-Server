ba862f4cda8c5c5199893b92239ee9312b45836d
HVDevelopers <147044768+HUMAIDV@users.noreply.github.com>
2026-04-10 23:35:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,3 @@ export const MongoDBUrl = 'mongodb+srv://valsadwalahumaid_db_user:qLdksEHl36GbEz
 // qLdksEHl36GbEz18
 
 export const BASE_URL = 'http://localhost:7000';
-
-export const AWS_BUCKET_NAME = 'hibloggers';
-
-export const AWS_ACCESS_KEY_ID = 'AKIAW3MEDAAR3LIE7RMN';
-
-export const AWS_SECRET_ACCESS_KEY = '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';
